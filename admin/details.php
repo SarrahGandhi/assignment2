7cdc0ec6dc148c88ae1da6a0e29ca2f66bd1c6df
Peiyu Han <hanpeiyu7755@gmail.com>
2025-03-30 18:13:23 -0400

    change images size for detail pages

--- admin/details.php
+++ admin/details.php
@@ -54,7 +54,7 @@ secure();
                             echo '<h1 class="display-4 mb-4">' . htmlspecialchars($recipe['RecipeName']) . '</h1>';
 
                             if (!empty($recipe['Photo'])) {
-                                echo '<img src="../' . htmlspecialchars($recipe['Photo']) . '" alt="Recipe Image" class="img-fluid mb-4 rounded shadow-sm">';
+                                echo '<img src="../' . htmlspecialchars($recipe['Photo']) . '" alt="Recipe Image" class="img-fluid mb-4 d-block mx-auto rounded shadow-sm" style="width: 650px; height: auto; object-fit: cover;">';
                             }
 
                             echo '<div class="card mb-4 shadow-sm">';
